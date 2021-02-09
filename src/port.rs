bc23c82101aa960fa430159dbfef4dc7faea3f71
Tarith <tarithj@gmail.com>
2021-02-09 18:23:12 +0530

    codeDoc: documented ScanBatch.entries

--- src/port.rs
+++ src/port.rs
@@ -1,5 +1,29 @@
-pub mod a {
-    pub struct Port {
+// scan entry
+pub struct ScanEntry {
+    result: bool
+}
+impl ScanEntry {
+    fn scan() {
 
+    }
+}
+
+
+// scan batch that contains scan entries
+pub struct ScanBatch {
+    // private scan entry slice
+    entries: [ScanEntry]
+}
+
+impl ScanBatch {
+    // adds scan entries into scan batch
+    pub fn addEntry(&mut self, e: ScanEntry) {
+        self.entries += e
+    }
+
+    pub fn run(self) {
+        for i in self.entries {
+            i
+        }
     }
 }
